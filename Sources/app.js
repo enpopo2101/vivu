7be6a3584e0f1fd61e0ee8fb621b68b87e9fe0eb
enpopo2101 <enpopo2101@gmail.com>
2018-12-21 23:45:47 +0700

    setting model mongoose

--- Sources/app.js
+++ Sources/app.js
@@ -1,41 +1,84 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+mongoose.connect('mongodb://localhost/vivu');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const VivuEventSchema = new Schema({
+  name: String
+})
+const VivuEvent = mongoose.model('VivuEvent', VivuEventSchema)
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const UserSchema = new Schema({
+  username: String,
+  password: String,
+  roles: [String] // admin, user
+})
+const User = mongoose.model('User', UserSchema)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', async function() {
+  console.log('connected')
+  await VivuEvent.deleteMany({})
+  await User.deleteMany({})
+  await VivuEvent.insertMany([
+    { name: 'Sự kiện 1'},
+    { name: 'Sự kiện 2'},
+    { name: 'Sự kiện 3'},
+  ])
+  await User.insertMany([
+    {
+      username: 'admin',
+      password: 'admin',
+      roles: ['admin']
+    },
+    {
+      username: 'user',
+      password: 'user',
+      roles: ['user']
+    },
+  ])
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-app.listen(5000, () => console.log('Server started listening on port 5000!'));
-module.exports = app;
+
+// const app = express()
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+// app.listen(5000, () => console.log('Server started listening on port 5000!'));
+// module.exports = app;
