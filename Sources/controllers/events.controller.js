0bea7042a644d569a4d40e25df596c08f198661c
enpopo2101 <enpopo2101@gmail.com>
2018-12-23 21:51:12 +0700

    app change

--- Sources/controllers/events.controller.js
+++ Sources/controllers/events.controller.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose')
 const VivuEvent = mongoose.model('VivuEvent')
-<<<<<<< HEAD
-exports.sendReqBody(function(req, res) {
-  console.log(req.body)
-})
-=======
->>>>>>> master
 exports.create(function(req, res) {
   var vivuEvent = new VivuEvent(req.body)
   vivuEvent.save(res.jsonp(vivuEvent))
